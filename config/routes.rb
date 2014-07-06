e2c6374c6418886446377fe14e14528fa57b726e
MrDanMacDonald <dan@danmacd.com>
2014-07-06 16:45:19 -0700

    add show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
